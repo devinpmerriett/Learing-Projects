1f8a4f29b097bb6b0e7001f345cd77644b60ed11
devinpmerriett <devinphilipmerriett@gmail.com>
2025-07-24 18:08:11 -0400

    CCA JS variables project

---
+++ kelvinWeather.js
@@ -0,0 +1,8 @@
+//Kelvin Conversion
+const kelvin = 0;
+//conversion to celcius 
+let celsius = kelvin -273;
+let fahreheit = Math.floor(celsius * (9/5) +32);
+
+
+console.log(`The temperature is ${fahreheit} degrees Fahrenheit`)
