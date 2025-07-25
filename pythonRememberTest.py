acc9d88bc73d99be23a90fd5b9b9230b121cbced
devinpmerriett <devinphilipmerriett@gmail.com>
2025-07-25 14:22:44 -0400

    test

---
+++ pythonRememberTest.py
@@ -0,0 +1,11 @@
+x = 1
+print(1)
+def summingFunction(x, y):
+    return x + y
+
+
+num1 = int(input("First number to be added: "))
+num2 = int(input('Second number to be added: '))
+
+
+print(summingFunction(num1,num2))
